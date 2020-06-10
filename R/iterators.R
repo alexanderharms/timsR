c08ce407bb4f7c4f05bea8f967ece9166e3189ce
Alexander Harms <a.g.j.harms@gmail.com>
2020-06-10 15:52:08 +0200

    Adds files

---
+++ R/iterators.R
@@ -0,0 +1,9 @@
+iterate_over_time <- function(tims_object) {
+
+  return(tims_object)
+}
+
+iterate_over_models <- function(tims_object) {
+
+  return(tims_object)
+}
---
+++ experiment_new.R
@@ -0,0 +1,8 @@
+# Load settings
+# Load data
+# Prepare time series
+# Iterate over models
+# Iterate over time
+# Calculate metrics
+# Export results
+# Plot results
